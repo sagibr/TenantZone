06a8f915dff32b02e0b06991b278aed80dcb7691
Sagi Barabi <sbarabi1@gmail.com>
2022-09-21 23:10:15 +0300

    home page

--- client/src/components/homePage/SearchBar.jsx
+++ client/src/components/homePage/SearchBar.jsx
@@ -114,13 +114,49 @@ const SearchBar = () => {
 
   return (
     <div>
-      <input
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value)
-        }}
-      ></input>
-      <button onClick={() => search()}>Search</button>
+      <label
+        for="default-search"
+        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+      >
+        Search
+      </label>
+      <div className="relative">
+        <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-gray-500 dark:text-gray-400"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            ></path>
+          </svg>
+        </div>
+        <input
+          type="search"
+          id="default-search"
+          className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+          placeholder="Search adress, rooms etc.. (example: 'shoham mitzpe 61, floor 3 4 rooms...')  "
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value)
+          }}
+          required
+        />
+        <button
+          type="submit"
+          className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800"
+          onClick={() => search()}
+        >
+          Search
+        </button>
+      </div>
     </div>
   )
 }
---
+++ client/src/components/searchResult/SingleApartment.jsx
@@ -0,0 +1,28 @@
+import "../apartmentProfile/apartment.css"
+const SingleApartment = (props) => {
+  return (
+    <div key={props.index} className="group relative">
+      <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+        <img
+          className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+          alt={props.adress}
+          src={props.image[0].url}
+        ></img>
+      </div>
+      <div className="mt-4 flex justify-between">
+        <div>
+          <h3 className="text-sm text-gray-700">
+            <a href={props.href}>
+              <span aria-hidden="true" className="absolute inset-0" />
+              {props.adress}
+            </a>
+          </h3>
+          <p className="mt-1 text-sm text-gray-500"></p>
+        </div>
+        <p className="text-sm font-medium text-gray-900">{props.price}</p>
+      </div>
+    </div>
+  )
+}
+
+export default SingleApartment
